6dc0caee55e468a1473897db27c1c9a77d8847bb
Dragan-Pavlovic <obuca.kalista@gmail.com>
2022-01-02 16:09:51 +0100

    stripe backend payment implemented

--- client/src/components/stripe-button/StripeCheckoutButton.jsx
+++ client/src/components/stripe-button/StripeCheckoutButton.jsx
@@ -1,13 +1,29 @@
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51KAuM4JRGos4wyqFd6W7SgQWaTSDtdMaXxPE0bUEo2RW6HD2evEUIO93n68p4r3dCaedwMRfWKFYCuAXOqBpUmhh00F4FhEmnO";
 
   const onToken = (token) => {
-    console.log(token);
-    alert(`Payment Successful`);
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        alert("Payment successful");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(
+          "There was an issue with your payment! Please use provided credit card."
+        );
+      });
   };
 
   return (
